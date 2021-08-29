9359d1a403c98b9735c53c4478941f8061caf753
Luana Stephany <ls.luanastephany@gmail.com>
2021-08-29 08:15:39 -0300

    criando componente footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Footer.scss'
+import { FaTwitter, FaFacebookSquare, FaLinkedin } from 'react-icons/fa'
+
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer-mobile">
+
+        <div className="icons-mobile">
+          <FaTwitter size={30} />
+          <FaFacebookSquare size={30} />
+          <FaLinkedin size={30} />
+        </div>
+        <div className="slogan">Just type what's on your mind and we'll </div>
+      </div>
+
+
+      <div className="footer-desktop">
+
+        <img className="logo-desktop" src="/img/logo.png" alt="" />
+
+        <div className="icons-desktop">
+          <FaTwitter size={30} />
+          <FaFacebookSquare size={30} />
+          <FaLinkedin size={30} />
+        </div>
+
+        <p className="copy-desktop">Copywright 2020 Bella Olonje.com</p>
+
+      </div>
+
+
+
+    </>
+  )
+}
+
+export default Footer
